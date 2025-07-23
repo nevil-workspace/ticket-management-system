2dfafede0a8262c95ce97c55a64991ad43b694d9
nevil-workspace <nevil.workspace11@gmail.com>
2025-07-23 16:04:49 +0530

    update base url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,10 @@ import './index.css';
 import axios from 'axios';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+axios.defaults.baseURL = process.env.VITE_API_URL || 'http://localhost:3000';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+console.log('Setting default axios url: ', axios.defaults.baseURL);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
